8cbc61af7dc447de25a5f8b9d6777271a023f014
gimchaewon <gimchaewon417@gmail.com>
2025-05-26 18:11:11 +0900

    7주차 미션

--- src/main/java/umc/study/repository/StoreRepository.java
+++
@@ -1,7 +0,0 @@
-package umc.study.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import umc.study.domain.Store;
-
-public interface StoreRepository extends JpaRepository<Store, Long>, StoreRepositoryCustom {
-}
--- src/main/java/umc/study/repository/StoreRepositoryCustom.java
+++
@@ -1,8 +0,0 @@
-package umc.study.repository;
-
-import umc.study.domain.Store;
-import java.util.List;
-
-public interface StoreRepositoryCustom {
-    List<Store> findOpenStoresByRegionId(Long regionId);
-}
